661f8389bb3f6ccb79705448f30b8059a99a1120
Tad Wohlrapp <git@wohlr.app>
2020-04-22 01:42:07 +0200

    1.0.1 Update Namespace and Author

--- show-image-dimensions.user.js
+++ show-image-dimensions.user.js
@@ -1,4 +1,5 @@
 // ==UserScript==
+// @namespace       https://openuserjs.org/users/Taddiboy
 // @name            Google Image Search - Show Image Dimensions
 // @name:de         Google Bildersuche - Bildabmessungen anzeigen
 // @name:fr         Google Image Search - Afficher les dimensions de l'image
@@ -13,66 +14,66 @@
 // @description:it  Visualizza le dimensioni dell'immagine (ad es. "1920 × 1080") per ogni miniatura nella pagina dei risultati della ricerca immagini di Google.
 // @description:pl  Wyświetla wymiary obrazu (np. "1920 × 1080") dla każdej miniaturki na stronie wyników wyszukiwania obrazów Google.
 // @description:ru  Отображает размеры изображения (например, "1920 × 1080") для каждой миниатюры на странице результатов поиска изображений Google.
-// @namespace       https://greasyfork.org/de/users/522821-taddiboy
-// @version         1.0.0
+// @copyright       2020, Taddiboy (https://openuserjs.org/users/Taddiboy)
 // @license         MIT
 // @author          Taddiboy
+// @version         1.0.1
 // @icon            https://i.imgur.com/7OeXVaf.png
 // @include         https://*.google.tld/*tbm=isch*
 // @grant           none
 // ==/UserScript==
 
 (function () {
-    'use strict';
-  
-    // Find all thumbnails
-    const images = document.querySelectorAll('[data-ow]');
-  
-    // Copy Google's own CSS used for image dimensions
-    const styles = `
-        .image-dimensions {
-          background-color: rgba(0,0,0,.5);
-          border-radius: 2px 0 0 0;
-          bottom: 0;
-          box-shadow: 0 0 1px 0 rgba(0,0,0,.16);
-          box-sizing: border-box;
-          color: #f1f3f4;
-          font-family: Roboto-Medium,Roboto,arial,sans-serif;
-          font-size: 10px;
-          right: 0;
-          line-height: 12px;
-          overflow: hidden;
-          padding: 4px;
-          position: absolute;
-          white-space: nowrap;
-        }
-      `;
-  
-    // Append stylesheet to the document
-    const styleSheet = document.createElement("style");
-    styleSheet.type = "text/css";
-    styleSheet.innerText = styles;
-    document.head.appendChild(styleSheet);
-  
-    // Loop through all thumbnails
-    for (let i = 0; i < images.length; i++) {
-      const image = images[i];
-  
-      // Get original width from 'data-ow' attribute
-      const width = image.getAttribute('data-ow');
-  
-      // Get original height from 'data-oh' attribute
-      const height = image.getAttribute('data-oh');
-  
-      // Create DIV and insert text
-      const dimensionsDiv = document.createElement("div");
-      const dimensionsContent = document.createTextNode(width + " × " + height);
-      dimensionsDiv.appendChild(dimensionsContent);
-  
-      // Assign CSS class
-      dimensionsDiv.classList.add("image-dimensions");
-  
-      // Append everything to thumbnail
-      image.firstChild.appendChild(dimensionsDiv);
-    }
-  })();  
+  'use strict';
+
+  // Find all thumbnails
+  const images = document.querySelectorAll('[data-ow]');
+
+  // Copy Google's own CSS used for image dimensions
+  const styles = `
+      .image-dimensions {
+        background-color: rgba(0,0,0,.5);
+        border-radius: 2px 0 0 0;
+        bottom: 0;
+        box-shadow: 0 0 1px 0 rgba(0,0,0,.16);
+        box-sizing: border-box;
+        color: #f1f3f4;
+        font-family: Roboto-Medium,Roboto,arial,sans-serif;
+        font-size: 10px;
+        right: 0;
+        line-height: 12px;
+        overflow: hidden;
+        padding: 4px;
+        position: absolute;
+        white-space: nowrap;
+      }
+    `;
+
+  // Append stylesheet to the document
+  const styleSheet = document.createElement("style");
+  styleSheet.type = "text/css";
+  styleSheet.innerText = styles;
+  document.head.appendChild(styleSheet);
+
+  // Loop through all thumbnails
+  for (let i = 0; i < images.length; i++) {
+    const image = images[i];
+
+    // Get original width from 'data-ow' attribute
+    const width = image.getAttribute('data-ow');
+
+    // Get original height from 'data-oh' attribute
+    const height = image.getAttribute('data-oh');
+
+    // Create DIV and insert text
+    const dimensionsDiv = document.createElement("div");
+    const dimensionsContent = document.createTextNode(width + " × " + height);
+    dimensionsDiv.appendChild(dimensionsContent);
+
+    // Assign CSS class
+    dimensionsDiv.classList.add("image-dimensions");
+
+    // Append everything to thumbnail
+    image.firstChild.appendChild(dimensionsDiv);
+  }
+})();
